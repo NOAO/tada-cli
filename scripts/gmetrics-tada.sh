376d415aaa0069e05ab116cf1dd97f8b6b008b09
Steve Pothier <pothier@noao.edu>
2015-08-19 12:41:07 -0700

    clean fake1.personality and gmetrics

--- scripts/gmetrics-tada.sh
+++ scripts/gmetrics-tada.sh
@@ -5,7 +5,6 @@ opts="--type=uint16 --units=size --group=tada"
 /usr/bin/gmetric $opts -n tada_inactive -v `/usr/bin/dqcli -c inactive` 
 /usr/bin/gmetric $opts -n tada_active   -v `/usr/bin/dqcli -c active`
 /usr/bin/gmetric $opts -n tada_records  -v `/usr/bin/dqcli -c records`
-/usr/bin/gmetric $opts -n tada_records  -v `/usr/bin/dqcli -c records`
 
 if [ "MOUNTAIN" = $hosttype ]; then
     # For MOUNTAIN
