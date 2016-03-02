9815f8b224a5e56dedd82d5737c34c5c0a6aea08
Steve Pothier <pothier@noao.edu>
2016-03-02 16:27:01 -0700

    snap

--- scripts/finished-log.sh
+++ scripts/finished-log.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 # PURPOSE: Waits until all lines in a manifest file show up in a log file.
-#   (or TIMEOUT is exceeded)
+#   (or TIMEOUT is exceeded). Manifest line may match PART of logfile line.
 #
 # EXAMPLE:
 
@@ -18,17 +18,19 @@ PROGRESS=0
 LOGFILE=/var/log/tada/pop.log
 TIMEOUT=30
 
-usage="USAGE: $cmd [options] match [match ...]
+usage="USAGE: $cmd [options] manifestFile
+manifestFile:  Strings (one per line) that must show up in logfile for success.
+               Each string can occur anywhere in any line of LOGFILE.
 OPTIONS:
   -l <logfile>:: File to read for matches (default=$LOGFILE)
-  -p <progress>:: Number of progress updates per second (default=0)
-  -t <seconds>:: Seconds to wait for match to show up in logfile (default=$TIMEOUT)
+  -t <seconds>:: Seconds to wait for all matches to show up in logfile 
+                 (default=$TIMEOUT)
   -v <verbosity>:: higher number for more output (default=0)
 
 "
 
 
-while getopts "hl:p:t:v:" opt; do
+while getopts "hl:t:v:" opt; do
     #!echo "opt=<$opt>"
     case $opt in
 	    h)
@@ -40,9 +42,6 @@ while getopts "hl:p:t:v:" opt; do
             ;;
         l)
             LOGFILE=$OPTARG
-            ;;
-        p)
-            PROGRESS=$OPTARG # how often to report progress
             ;;
         t)
             TIMEOUT=$OPTARG
@@ -87,11 +86,10 @@ function echonverbose () {
 
 maxTries=$TIMEOUT
 readarray strings < $MANIFEST
-for str in "${strings[@]%?}"
-do           
+tries=0
+for str in "${strings[@]%?}"; do
     echoverbose "Looking in logfile for: \"$str\""
     echoverbose "# Waiting $maxTries seconds for $str in '$LOGFILE'"
-    tries=0
     echonverbose -n "# wait($maxTries)"
     while ! grep -F "$str" $LOGFILE > /dev/null; do
 	    tries=$((tries+1))
@@ -99,7 +97,7 @@ do
 	    if [ "$tries" -gt "$maxTries" ]; then
             echoverbose ""
 	        echo
-	        echo "Aborted after maxTries=$maxTries: \"$str\" NOT FOUND"
+	        echo "Aborted after $maxTries seconds: \"$str\" NOT FOUND"
 	        exit 1
 	    fi
 	    sleep 1
