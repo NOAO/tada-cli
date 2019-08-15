9afdafd7f798fc125a383c28334d142c052533e7
Sean McManus <sgmcmanus@gmail.com>
2019-08-15 12:05:46 -0700

    update ganglia script

--- scripts/gmetrics-tada.sh
+++ scripts/gmetrics-tada.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-hosttype=${1:-MOUNTAIN}
+hosttype=${1:-VALLEY}
 if [ ! -x /usr/bin/gmetric ]; then
     exit
 fi
@@ -7,28 +7,9 @@ fi
 source /opt/tada/venv/bin/activate
 
 opts="--type=uint16 --units=size --group=tada"
-personalities="pipeline-decam pipeline-mosaic3 pipeline-90prime"
+personalities="pipeline-decam pipeline-mosaic3 pipeline-90prime ct4m-decam"
 
 for personality in $personalities; do
-  #/usr/bin/gmetric $opts -n tada_inactive -v `dqcli -c inactive`
   /usr/bin/gmetric $opts -n tada_active_${personality} -v $(dqcli --list active | grep $personality | wc -l)
-  #/usr/bin/gmetric $opts -n tada_records  -v `dqcli -c records`
 done
-
-exit
-
-if [ "MOUNTAIN" = $hosttype ]; then
-    # For MOUNTAIN
-    val=`find /var/tada/mountain_cache -type f | wc -l`
-    /usr/bin/gmetric $opts -n tada_pending_xfer -v $val
-    val=`find /var/tada/mountain_stash -type f | wc -l`
-    /usr/bin/gmetric $opts -n tada_stashed -v $val
-else
-    # For VALLEY
-    val=`find /var/tada/mountain-mirror -type f | wc -l`
-    /usr/bin/gmetric $opts -n tada_pending_ingest  -v $val
-    val=`find /var/tada/noarchive -type f | wc -l`
-    /usr/bin/gmetric $opts -n tada_noarchive  -v $val
-fi
-
 
