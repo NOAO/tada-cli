6edc3b536abda84a6dbaa7562257a9e3d1de4572
Sean McManus <sgmcmanus@gmail.com>
2019-08-15 12:49:53 -0700

    add ct4m-decam to ganglia monitor

--- scripts/gmetrics-tada.sh
+++ scripts/gmetrics-tada.sh
@@ -7,9 +7,27 @@ fi
 source /opt/tada/venv/bin/activate
 
 opts="--type=uint16 --units=size --group=tada"
-personalities="pipeline-decam pipeline-mosaic3 pipeline-90prime ct4m-decam"
+list="pipeline-decam pipeline-mosaic3 pipeline-90prime"
 
-for personality in $personalities; do
-  /usr/bin/gmetric $opts -n tada_active_${personality} -v $(dqcli --list active | grep $personality | wc -l)
+for personality in $list; do
+
+  DROPBOX=$(find /var/tada/dropbox/ -type f -path "*/$personality/*" -mmin -60 | wc -l)
+  ACTIVE=$(dqcli --list active | grep $personality | wc -l)
+
+  /usr/bin/gmetric $opts -n tada_dropbox_${personality} -v $DROPBOX
+  /usr/bin/gmetric $opts -n tada_active_${personality} -v $ACTIVE
+
 done
 
+list="ct4m-decam"
+for personality in $list; do
+
+  NOWATCH=$(find /var/tada/nowatch/ -type f -path "*/$personality/*" -mmin -60 | wc -l)
+  DROPBOX=$(find /var/tada/dropbox/ -type f -path "*/$personality/*" -mmin -60 | wc -l)
+  ACTIVE=$(dqcli --list active | grep $personality | wc -l)
+
+  /usr/bin/gmetric $opts -n tada_nowatch_${personality} -v $NOWATCH
+  /usr/bin/gmetric $opts -n tada_dropbox_${personality} -v $DROPBOX
+  /usr/bin/gmetric $opts -n tada_active_${personality} -v $ACTIVE
+
+done
