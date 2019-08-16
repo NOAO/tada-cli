0065c2e2d9b9139a757372d4dac3410ba95965a9
Sean McManus <sgmcmanus@gmail.com>
2019-08-15 19:16:03 -0700

    update ganglia script

--- scripts/gmetrics-tada.sh
+++ scripts/gmetrics-tada.sh
@@ -7,14 +7,16 @@ fi
 source /opt/tada/venv/bin/activate
 
 opts="--type=uint16 --units=size --group=tada"
+export TZ="America/Santiago"
+OBSNIGHT=$(date --date "0 days -12 hours 0 seconds" +%Y%m%d)
 
 # La Serena Intermediate Hop for DECam xfers.
 if [ $(hostname) == "valls2.ctio.noao.edu" ]; then
-  COUNT=$(find /var/tada/nowatch/ -type f -path "*/ct4m-decam/*" -mmin -60 | wc -l)
+  COUNT=$(find /var/tada/nowatch/${OBSNIGHT}/ct4m-decam/ -type f -name "*.fz" | wc -l)
   /usr/bin/gmetric $opts -n ct4m-decam-xfer-laserena -v $COUNT
 # Tucson dropbox and ingest for DECam. 
 elif [ $(hostname) == "valtu1.sdm.noao.edu" ]; then
-  COUNT=$(find /var/tada/dropbox/ -type f -path "*/ct4m-decam/*" -mmin -60 | wc -l)
+  COUNT=$(find /var/tada/dropbox/${OBSNIGHT}/ct4m-decam/ -type f -name "*.fz" | wc -l)
   /usr/bin/gmetric $opts -n ct4m-decam-xfer-tucson-incoming -v $COUNT
   COUNT=$(dqcli --list active | grep ct4m-decam | wc -l)
   /usr/bin/gmetric $opts -n ct4m-decam-xfer-tucson-ingestbacklog -v $COUNT
