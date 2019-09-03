a1f9c90172fc119b923618446d81c7dfc7735827
Steve Pothier <pothier@noao.edu>
2019-09-03 14:43:24 -0700

    add OVERWRITE and DTACQNAM

--- scripts/add_params_yaml.sh
+++ scripts/add_params_yaml.sh
@@ -2,6 +2,11 @@
 # PURPOSE: 
 #   Add min personality containing FILENAME, MD5SUM to dir containing FITS file.
 #
+# EXAMPLE:
+#  find `pwd` -name "*.fits.fz" -exec scripts/add_params_yaml.sh {} \;
+#  find `pwd` -name "*.yaml" -exec grep DTACQNAM {} \;
+#  find `pwd` -name "*.yaml" -exec grep overwrite {} \;
+
 cmd=`basename $0`
 usage="USAGE: $cmd [options] absPathToLocalFITS"
 
@@ -16,14 +21,21 @@ localfits=$1
 
 ##############################################################################
 
-md5=`md5sum ${localfits} | cut -d' ' -f1`
-val=`fitsheader -k DTACQNAM -t ascii.csv psg_161230_061637_ori_tTADASMOKE.fits.fz| tail -1`
-IFS=',' read -a KW <<< "$val"
-origfits=${KW[3]}
+yaml=${localfits}.yaml
+echo "Creating YAML: ${yaml}"
 
-cat > ${localfits}.yaml <<EOF
+md5=`md5sum ${localfits} | cut -d' ' -f1`
+val=`fitsheader -k DTACQNAM -t ascii.csv ${localfits} | tail -1` 
+IFS=',' read -a KW <<< "$val"
+dtacqnam=${KW[3]}
+origfits=${dtacqnam:-$localfits}
+
+cat > ${yaml} <<EOF
+options:
+    DTACQNAM: $origfits
 params:
     filename: $origfits
     md5sum: $md5
+    overwrite: True
 EOF
 
