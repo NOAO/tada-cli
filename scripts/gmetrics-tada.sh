59e1257cbba61ec378023e958ab6a21d574e2c92
Sean McManus <mcmanus@ocean.tuc.noao.edu>
2020-02-27 14:10:29 -0700

    snap

--- scripts/gmetrics-tada.sh
+++ scripts/gmetrics-tada.sh
@@ -12,20 +12,20 @@ OBSNIGHT=$(date --date "0 days -12 hours 0 seconds" +%Y%m%d)
 
 # La Serena Intermediate Hop for DECam xfers.
 if [ $(hostname) == "valls2.ctio.noao.edu" ]; then
-  COUNT=$(find /var/tada/nowatch/${OBSNIGHT}/ct4m-decam/ -type f -name "*.fz" | wc -l)
+  COUNT=$(find /var/tada/nowatch/${OBSNIGHT}/ct4m-decam/ -type f -name "*.fz" 2> /dev/null | wc -l)
   /usr/bin/gmetric $opts -n ct4m-decam-xfer-laserena -v $COUNT
 # Tucson dropbox and ingest for DECam. 
 elif [ $(hostname) == "valtu1.sdm.noao.edu" ]; then
-  COUNT=$(find /var/tada/dropbox/${OBSNIGHT}/ct4m-decam/ -type f -name "*.fz" | wc -l)
+  COUNT=$(find /var/tada/dropbox/${OBSNIGHT}/ct4m-decam/ -type f -name "*.fz" 2> /dev/null | wc -l)
   /usr/bin/gmetric $opts -n ct4m-decam-xfer-tucson-incoming -v $COUNT
-  COUNT=$(find /net/archive/mtn/${OBSNIGHT}/ct4m/ -type f -name "*.fz" | wc -l)
+  COUNT=$(find /net/archive/mtn/${OBSNIGHT}/ct4m/ -type f -name "*.fz" 2> /dev/null | wc -l)
   /usr/bin/gmetric $opts -n ct4m-decam-xfer-tucson-archived -v $COUNT
   COUNT=$(dqcli --list active | grep ct4m-decam | wc -l)
   /usr/bin/gmetric $opts -n ct4m-decam-xfer-tucson-ingestbacklog -v $COUNT
 else
   list="pipeline-decam pipeline-mosaic3 pipeline-90prime"
   for personality in $list; do
-    DROPBOX=$(find /var/tada/dropbox/ -type f -path "*/$personality/*" -mmin -60 | wc -l)
+    DROPBOX=$(find /var/tada/dropbox/ -type f -path "*/$personality/*" -mmin -60 2> /dev/null | wc -l)
     ACTIVE=$(dqcli --list active | grep $personality | wc -l)
     /usr/bin/gmetric $opts -n tada_dropbox_${personality} -v $DROPBOX
     /usr/bin/gmetric $opts -n tada_active_${personality} -v $ACTIVE
